1cdc38bd525c7a35744902e4379ac6bdfb78c6cd
kurmachu <flyingkurbypikmin@gmail.com>
2019-07-17 13:17:41 -0700

    Update values to 2.5

--- src/main/java/corp/kamabo/thebackrooms/BackroomMod.java
+++ src/main/java/corp/kamabo/thebackrooms/BackroomMod.java
@@ -27,7 +27,7 @@ import org.apache.logging.log4j.Logger;
 public class BackroomMod {
     public static final String MODID = "thebackrooms";
     public static final String NAME = "The Backrooms";
-    public static final String VERSION = "2.4";
+    public static final String VERSION = "2.5";
 
     private static Logger logger;
 
