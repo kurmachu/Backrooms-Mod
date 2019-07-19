f303f9a14d5edc27d3224b8ed12af19276bf1c6e
kurmachu <flyingkurbypikmin@gmail.com>
2019-07-19 12:42:16 -0700

    Revert "Make the pebble block only Horizontal"
    This reverts commit f271efbae2467d37ba62a49fe984de93eaf0823a.

--- src/main/java/corp/kamabo/thebackrooms/block/BlockPipeValve.java
+++ src/main/java/corp/kamabo/thebackrooms/block/BlockPipeValve.java
@@ -3,7 +3,6 @@ package corp.kamabo.thebackrooms.block;
 import corp.kamabo.thebackrooms.BackroomMod;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockDirectional;
-import net.minecraft.block.BlockHorizontal;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.IProperty;
@@ -21,7 +20,7 @@ import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class BlockPipeValve extends Block {
-    public static final PropertyDirection FACING = BlockHorizontal.FACING;
+    public static final PropertyDirection FACING = BlockDirectional.FACING;
 
     public BlockPipeValve() {
         super(Material.ROCK);
