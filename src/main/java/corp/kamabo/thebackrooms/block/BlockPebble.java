56697b6274fa3e15dc790adf3ae207ce8ed77878
kurmachu <flyingkurbypikmin@gmail.com>
2019-07-19 19:12:31 -0700

    Make pebbles work in survival

--- src/main/java/corp/kamabo/thebackrooms/block/BlockPebble.java
+++ src/main/java/corp/kamabo/thebackrooms/block/BlockPebble.java
@@ -11,8 +11,11 @@ import net.minecraft.block.properties.PropertyDirection;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.NonNullList;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
@@ -22,18 +25,18 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 
 import javax.annotation.Nullable;
 import java.util.List;
+import java.util.Random;
 
 public class BlockPebble extends Block {
     public static final PropertyDirection FACING = BlockHorizontal.FACING;
 
     public BlockPebble() {
-        super(Material.ROCK);
+        super(Material.GROUND);
         setRegistryName("pebbles");
         setUnlocalizedName("pebbles");
         setSoundType(SoundType.STONE);
-        setHarvestLevel("pickaxe", 1);
-//        setHardness(5000f);
-//        setResistance(5000f);
+        setHardness(0.1f);
+        setResistance(1f);
         setLightLevel(0F);
         setLightOpacity(0);
         setCreativeTab(BackroomMod.creativeTab);
@@ -41,10 +44,17 @@ public class BlockPebble extends Block {
         this.setDefaultState(this.blockState.getBaseState().withProperty(FACING, EnumFacing.NORTH));
     }
 
+
     @SideOnly(Side.CLIENT)
     @Override
     public BlockRenderLayer getBlockLayer() {
         return BlockRenderLayer.CUTOUT;
+    }
+
+    @Nullable
+    @Override
+    public String getHarvestTool(IBlockState state) {
+        return null;
     }
 
     @Override
@@ -75,6 +85,19 @@ public class BlockPebble extends Block {
     @Override
     public void addCollisionBoxToList(IBlockState state, World worldIn, BlockPos pos, AxisAlignedBB entityBox, List<AxisAlignedBB> collidingBoxes, @Nullable Entity entityIn, boolean isActualState) {
 
+    }
+
+    @Override
+    public int quantityDropped(Random random) {
+        if (random.nextInt(100)<30){
+            return 2;
+        }
+        return 1;
+    }
+
+    @Override
+    public Item getItemDropped(IBlockState state, Random rand, int fortune) {
+        return Item.getItemFromBlock(this);
     }
 
     @Override
