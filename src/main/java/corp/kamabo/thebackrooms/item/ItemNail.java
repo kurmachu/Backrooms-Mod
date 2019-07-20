9365e7ae39fbcc7228db46651860c261ea7d84ff
kurmachu <flyingkurbypikmin@gmail.com>
2019-07-19 22:04:16 -0700

    Create ItemNail.java

---
+++ src/main/java/corp/kamabo/thebackrooms/item/ItemNail.java
@@ -0,0 +1,12 @@
+package corp.kamabo.thebackrooms.item;
+
+import corp.kamabo.thebackrooms.BackroomMod;
+import net.minecraft.item.Item;
+
+public class ItemNail extends Item {
+    public ItemNail(){
+        setUnlocalizedName("nail");
+        setRegistryName("nail");
+        setCreativeTab(BackroomMod.creativeTab);
+    }
+}
