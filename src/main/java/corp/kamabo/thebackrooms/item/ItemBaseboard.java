b045bb17d64954ac079d17918cf11028ded44482
kurmachu <flyingkurbypikmin@gmail.com>
2019-07-19 23:25:11 -0700

    Baseboards burn

--- src/main/java/corp/kamabo/thebackrooms/item/ItemBaseboard.java
+++ src/main/java/corp/kamabo/thebackrooms/item/ItemBaseboard.java
@@ -2,11 +2,17 @@ package corp.kamabo.thebackrooms.item;
 
 import corp.kamabo.thebackrooms.BackroomMod;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 
 public class ItemBaseboard extends Item {
     public ItemBaseboard(){
         setUnlocalizedName("baseboard");
         setRegistryName("baseboard");
         setCreativeTab(BackroomMod.creativeTab);
+    }
+
+    @Override
+    public int getItemBurnTime(ItemStack itemStack) {
+        return 400;
     }
 }
